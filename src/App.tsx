6d1286496fe975d28d0f583adafe37b0c0fa8aaa
Anthony Racioppo <ajrac@udel.edu>
2021-09-30 19:57:14 -0400

    Added questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const questions = [
+		{
+			questionText: "What is my dog's name?",
+			answerOptions: [
+				{ answerText: 'Fido', isCorrect: false },
+				{ answerText: 'Maggie', isCorrect: true },
+				{ answerText: 'Spot', isCorrect: false },
+				{ answerText: 'Bella', isCorrect: false },
+			],
+		},
+		{
+			questionText: "What is 2+2?",
+			answerOptions: [
+				{ answerText: '4', isCorrect: true },
+				{ answerText: '5', isCorrect: false },
+				{ answerText: '3', isCorrect: false },
+				{ answerText: '9', isCorrect: false },
+			],
+		},
+		{
+			questionText: "Why did the chicken cross the road?",
+			answerOptions: [
+				{ answerText: 'Because he wanted takeout', isCorrect: false },
+				{ answerText: 'Because 7 ate 9', isCorrect: false },
+				{ answerText: 'To get to the other side', isCorrect: true },
+				{ answerText: 'To catch his cab', isCorrect: false },
+			],
+		},
+		{
+			questionText: "What is my name?",
+			answerOptions: [
+				{ answerText: 'Anthony', isCorrect: true },
+				{ answerText: 'Joe', isCorrect: false },
+				{ answerText: 'John', isCorrect: false },
+				{ answerText: 'Matt', isCorrect: false },
+			],
+		},
+    {
+			questionText: "What is 4x9?",
+			answerOptions: [
+				{ answerText: '32', isCorrect: false },
+				{ answerText: '35', isCorrect: false },
+				{ answerText: '36', isCorrect: true },
+				{ answerText: '512', isCorrect: false },
+			],
+		},
+	];
 }
-
 export default App;
